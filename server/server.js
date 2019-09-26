190868290cae63b5617a87883f65317223269e18
tjlee527 <tjlee527@gmail.com>
2019-09-26 14:43:35 -0700

    modified to serve detail ec2

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const nr = require('newrelic');
 const express = require('express');
 const proxy = require('http-proxy-middleware');
 
@@ -5,9 +6,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-app.use(proxy('/api/images', {target: 'http://localhost:3003/'}));
-app.use(proxy('/api/checkout', {target: 'http://localhost:3002/'}));
-app.use(proxy('/api/product', {target: 'http://localhost:3004/'}));
-app.use(proxy('/api/', {target: 'http://localhost:3001/'}));
+// app.use(proxy('/api/images', {target: 'http://localhost:3003/'}));
+// app.use(proxy('/api/checkout', {target: 'http://localhost:3002/'}));
+// app.use(proxy('/api/product', {target: 'http://localhost:3004/'}));
+app.use(proxy('/api/', {target: 'ec2-54-153-18-154.us-west-1.compute.amazonaws.com'}));
 
 app.listen(port, () => console.log('listening to port 3000'));
